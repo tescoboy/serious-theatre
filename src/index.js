5e346f6a3f51395efa96a484afe124005af2cafa
tescoboy <43451876+tescoboy@users.noreply.github.com>
2025-01-02 11:15:59 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TheatreApp from './App';
-import './index.css'; // Add CSS if needed
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <TheatreApp />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
