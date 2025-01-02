22bb03e8aa27396cbc1b31a4a8505aa721f083a7
tescoboy <43451876+tescoboy@users.noreply.github.com>
2025-01-02 10:31:02 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TheatreApp from './App';
+import './index.css'; // Add CSS if needed
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TheatreApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
